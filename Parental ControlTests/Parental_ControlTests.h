b3555783441df956741a5453b5da7fdb2154bbe2
Michael Sambol <Michael@lawn-143-215-123-115.lawn.gatech.edu>
2013-02-15 11:22:07 -0500

    test2

---
+++ Parental ControlTests/Parental_ControlTests.h
@@ -0,0 +1,13 @@
+//
+//  Parental_ControlTests.h
+//  Parental ControlTests
+//
+//  Created by Michael Sambol on 1/14/13.
+//  Copyright 2013 __MyCompanyName__. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface Parental_ControlTests : SenTestCase
+
+@end
