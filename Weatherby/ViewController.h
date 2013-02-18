de908606d694e9c0876388414f82dfc46765d911
user2492 <user2492@l062.macincloud.com>
2013-02-18 14:58:21 -0800

    Test

---
+++ Weatherby/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Weatherby
+//
+//  Created by user2492 on 2/18/13.
+//  Copyright (c) 2013 Weatherby Foundation. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ Weatherby/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Weatherby
+//
+//  Created by user2492 on 2/18/13.
+//  Copyright (c) 2013 Weatherby Foundation. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
