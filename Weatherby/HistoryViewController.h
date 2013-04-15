08ce78184375bf4ba2b33cc296026729ce4b9073
bruce <cgbrucezjy@aol.com>
2013-04-15 16:43:59 -0400

    adding HistoryViewController.

---
+++ Weatherby/HistoryViewController.h
@@ -0,0 +1,13 @@
+//
+//  HistoryViewController.h
+//  Weatherby
+//
+//  Created by ruan chao on 13-4-15.
+//  Copyright (c) 2013年 Weatherby Foundation. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface HistoryViewController : UIViewController
+
+@end
---
+++ Weatherby/HistoryViewController.m
@@ -0,0 +1,13 @@
+//
+//  HistoryViewController.m
+//  Weatherby
+//
+//  Created by ruan chao on 13-4-15.
+//  Copyright (c) 2013年 Weatherby Foundation. All rights reserved.
+//
+
+#import "HistoryViewController.h"
+
+@implementation HistoryViewController
+
+@end
