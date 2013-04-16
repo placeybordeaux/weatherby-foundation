1735f8dd82b93b8ca2a96c15c2273c8d39966ba6
aronijin <aronijin@gmail.com>
2013-04-16 13:20:13 -0700

    Added update checking for Winners and Conservation Effrots.

--- Weatherby/ConservationViewController.m
+++ Weatherby/ConservationViewController.m
@@ -31,19 +31,55 @@
     self.localSearchBar.delegate = self;
     self.localTableView.delegate = self;
     
-    // Download and write to file
-    NSString *fp = [[NSBundle mainBundle] pathForResource:@"Efforts" ofType:@"txt" inDirectory:nil];
-    NSURL *u = [NSURL URLWithString:@"https://www.dropbox.com/s/k96vax1cgb9pqo2/Efforts.txt?dl=1"];
-    NSData *ud = [NSData dataWithContentsOfURL:u];
-    [ud writeToFile:fp atomically:YES];
-   
-    //[[DBAccountManager sharedManager] linkFromController:@"kDBRootDropbox"];
-    NSString *filePath = [[NSBundle mainBundle] pathForResource:@"ConservationEfforts" ofType:@"txt" inDirectory:nil];
+    Boolean con = false;
+    NSArray *dateDownloaded;
+    NSArray *dateNow;
     
     // Download and write to file
-    NSURL *url = [NSURL URLWithString:@"https://www.dropbox.com/s/mevknt61g75osdw/ConservationEfforts.txt?dl=1"];
-    NSData *urlData = [NSData dataWithContentsOfURL:url];
-    [urlData writeToFile:filePath atomically:YES];
+    NSString *fp = [[NSBundle mainBundle] pathForResource:@"Efforts" ofType:@"txt" inDirectory:nil];
+    NSString *filePath = [[NSBundle mainBundle] pathForResource:@"ConservationEfforts" ofType:@"txt" inDirectory:nil];
+    NSString *path = [[NSBundle mainBundle] pathForResource:@"UpdateCon" ofType:@"txt" inDirectory:nil];
+    NSString *localContent = [NSString stringWithContentsOfFile:path
+                                                       encoding:NSUTF8StringEncoding
+                                                          error:NULL];
+    
+    // Get update timestamp from DROPBOX
+    NSURL *updateURL = [NSURL URLWithString:@"https://www.dropbox.com/s/iqrinhe92alj12r/UpdateCon.txt?dl=1"];
+    NSData *updateData = [NSData dataWithContentsOfURL:updateURL];
+    [updateData writeToFile:path atomically:YES];
+    NSString* downloadedContent = [NSString stringWithContentsOfFile:path
+                                                            encoding:NSUTF8StringEncoding
+                                                               error:NULL];
+    //NSLog(@"%@\n", localContent);
+    //NSLog(@"%@\n", downloadedContent);
+    // Grab date
+    dateDownloaded = [downloadedContent  componentsSeparatedByString: @"-"];
+    dateNow = [localContent componentsSeparatedByString:@"-"];
+    
+    // Compate by year, month, day
+    if ([[dateDownloaded objectAtIndex:0] intValue] > [[dateNow objectAtIndex:0] intValue]) {
+        // True
+        con = true;
+    } else if ([[dateDownloaded objectAtIndex:0] intValue] == [[dateNow objectAtIndex:0] intValue] && [[dateDownloaded objectAtIndex:1] intValue] > [[dateNow objectAtIndex:1] intValue]) {
+        // True
+        con = true;
+    } else if ([[dateDownloaded objectAtIndex:0] intValue] == [[dateNow objectAtIndex:0] intValue] && [[dateDownloaded objectAtIndex:1] intValue] == [[dateNow objectAtIndex:1] intValue] && [[dateDownloaded objectAtIndex:2] intValue] > [[dateNow objectAtIndex:2] intValue]) {
+        // True
+        con = true;
+    }
+    
+ 
+    if (con) {
+        NSURL *u = [NSURL URLWithString:@"https://www.dropbox.com/s/k96vax1cgb9pqo2/Efforts.txt?dl=1"];
+        NSData *ud = [NSData dataWithContentsOfURL:u];
+        [ud writeToFile:fp atomically:YES];
+    
+        // Download and write to file
+        NSURL *url = [NSURL URLWithString:@"https://www.dropbox.com/s/mevknt61g75osdw/ConservationEfforts.txt?dl=1"];
+        NSData *urlData = [NSData dataWithContentsOfURL:url];
+        [urlData writeToFile:filePath atomically:YES];
+    }
+
     NSString* content = [NSString stringWithContentsOfFile:filePath
                                                   encoding:NSUTF8StringEncoding
                                                      error:NULL];
--- Weatherby/WinnersViewController.m
+++ Weatherby/WinnersViewController.m
@@ -28,24 +28,60 @@
     [super viewDidLoad];
     self.localSearchBar.delegate = self;
     self.localTableView.delegate = self;
+    Boolean win = false;
+    NSArray *dateDownloaded;
+    NSArray *dateNow;
     
-    // Download and write to file
+    // Get paths
     NSString *fp = [[NSBundle mainBundle] pathForResource:@"WinnerBios" ofType:@"txt" inDirectory:nil];
-    NSURL *u = [NSURL URLWithString:@"https://www.dropbox.com/s/4xryuumf6xr3itq/WinnerBios.txt?dl=1"];
-    NSData *ud = [NSData dataWithContentsOfURL:u];
-    [ud writeToFile:fp atomically:YES];    
-
-    //[[DBAccountManager sharedManager] linkFromController:@"kDBRootDropbox"];
     NSString *filePath = [[NSBundle mainBundle] pathForResource:@"Winners" ofType:@"txt" inDirectory:nil];
+    NSString *path = [[NSBundle mainBundle] pathForResource:@"UpdateWin" ofType:@"txt" inDirectory:nil];
+    NSString *localContent = [NSString stringWithContentsOfFile:path
+                                                       encoding:NSUTF8StringEncoding
+                                                          error:NULL];
     
-    // Download and write to file
-    NSURL *url = [NSURL URLWithString:@"https://dl.dropbox.com/s/itoeklwyqnvtk6y/Winners.txt?dl=1"];
-    NSData *urlData = [NSData dataWithContentsOfURL:url];
-    [urlData writeToFile:filePath atomically:YES];
+    // Get update timestamp from DROPBOX
+    NSURL *updateURL = [NSURL URLWithString:@"https://www.dropbox.com/s/fbx11okcyca5s1k/UpdateWin.txt?dl=1"];
+    NSData *updateData = [NSData dataWithContentsOfURL:updateURL];
+    [updateData writeToFile:path atomically:YES];
+    NSString* downloadedContent = [NSString stringWithContentsOfFile:path
+                                                            encoding:NSUTF8StringEncoding
+                                                               error:NULL];
+    //NSLog(@"%@\n", localContent);
+    //NSLog(@"%@\n", downloadedContent);
+    // Grab date
+    dateDownloaded = [downloadedContent  componentsSeparatedByString: @"-"];
+    dateNow = [localContent componentsSeparatedByString:@"-"];
+    
+    // Compate by year, month, day
+    if ([[dateDownloaded objectAtIndex:0] intValue] > [[dateNow objectAtIndex:0] intValue]) {
+        // True
+        win = true;
+    } else if ([[dateDownloaded objectAtIndex:0] intValue] == [[dateNow objectAtIndex:0] intValue] && [[dateDownloaded objectAtIndex:1] intValue] > [[dateNow objectAtIndex:1] intValue]) {
+        // True
+        win = true;
+    } else if ([[dateDownloaded objectAtIndex:0] intValue] == [[dateNow objectAtIndex:0] intValue] && [[dateDownloaded objectAtIndex:1] intValue] == [[dateNow objectAtIndex:1] intValue] && [[dateDownloaded objectAtIndex:2] intValue] > [[dateNow objectAtIndex:2] intValue]) {
+        // True
+        win = true;
+    }
+    
+    if (win) {
+        //Download and write files
+        NSURL *u = [NSURL URLWithString:@"https://www.dropbox.com/s/4xryuumf6xr3itq/WinnerBios.txt?dl=1"];
+        NSData *ud = [NSData dataWithContentsOfURL:u];
+        [ud writeToFile:fp atomically:YES];
+        
+        // Download and write to file
+        NSURL *url = [NSURL URLWithString:@"https://dl.dropbox.com/s/itoeklwyqnvtk6y/Winners.txt?dl=1"];
+        NSData *urlData = [NSData dataWithContentsOfURL:url];
+        [urlData writeToFile:filePath atomically:YES];
+    }
+    
+    
     NSString* content = [NSString stringWithContentsOfFile:filePath
                                                   encoding:NSUTF8StringEncoding
                                                      error:NULL];
-
+    
     // Break down string and store it into the array
     unfilteredWinners = [content componentsSeparatedByString: @"\n"];
     [self fillTable:unfilteredWinners];
